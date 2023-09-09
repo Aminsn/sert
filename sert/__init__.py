cce30b7ae40080027a873caa22b5048858d5a9d0
aminsn <aminsh2010@gmail.com>
2023-09-09 09:46:41 +0100

    version number fixed

--- sert/__init__.py
+++ sert/__init__.py
@@ -6,4 +6,4 @@ Deep learning on sets for Python
 Sert is a Python package that provides a set of tools and models to learn from data in the form of sets.
 """
 
-__version__ = '0.1.0'
+__version__ = '0.0.1'
